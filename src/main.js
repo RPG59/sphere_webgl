15bd8eae560ee06c03e2555a6c432773c9f1b5e7
KanshinIS <kanshinis@skblab.ru>
2019-09-16 07:44:21 +0300

    update

--- src/main.js
+++ src/main.js
@@ -39,6 +39,37 @@ class Shader {
     }
 }
 
+
+class Texture {
+    constructor(path) {
+        this.texture = gl.createTexture();
+        this.image = new Image();
+        this.image.src = path;
+        document.body.appendChild(this.image);
+    }
+
+    create() {
+        return new Promise(res => {
+            this.image.addEventListener('load', () => {
+                gl.bindTexture(gl.TEXTURE_2D, this.texture);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
+                res();
+            });
+        });
+    }
+
+    bind() {
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    }
+
+    unbind() {
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    }
+
+}
+
 class float4x4 {
     constructor() {
         this.elements = new Float32Array([
@@ -60,6 +91,41 @@ class float4x4 {
 
         return this;
 
+    }
+
+    rotateFromQuatertion(quat) {
+        let qx, qy, qz, qw, qx2, qy2, qz2, qxqx2, qyqy2, qzqz2, qxqy2, qyqz2, qzqw2, qxqz2, qyqw2, qxqw2;
+        qx = quat.x;
+        qy = quat.y;
+        qz = quat.z;
+        qw = quat.w;
+        qx2 = (qx + qx);
+        qy2 = (qy + qy);
+        qz2 = (qz + qz);
+        qxqx2 = (qx * qx2);
+        qxqy2 = (qx * qy2);
+        qxqz2 = (qx * qz2);
+        qxqw2 = (qw * qx2);
+        qyqy2 = (qy * qy2);
+        qyqz2 = (qy * qz2);
+        qyqw2 = (qw * qy2);
+        qzqz2 = (qz * qz2);
+        qzqw2 = (qw * qz2);
+
+        this.elements[0] = (1 - qyqy2) - qzqz2;
+        this.elements[1] = qxqy2 - qzqw2;
+        this.elements[2] = qxqz2 + qyqw2;
+        this.elements[3] = 0;
+
+        this.elements[4] = qxqy2 - qzqw2;
+        this.elements[5] = (1 - qxqx2) - qzqz2;
+        this.elements[6] = qyqz2 - qxqw2;
+        this.elements[7] = 0;
+
+        this.elements[8] = qxqz2 - qyqw2;
+        this.elements[9] = qyqz2 + qxqw2;
+        this.elements[10] = (1 - qxqx2) - qyqy2;
+        this.elements[11] = 0;
     }
 
     lookAt(pos, at, up) {
@@ -114,6 +180,24 @@ class float4x4 {
     };
 }
 
+class float4 {
+    constructor(x = 0, y = 0, z = 0, w = 0) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+}
+
+class float3 {
+    constructor(x = 0, y = 0, z = 0) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+
 class VertexArray {
     constructor(vertices, indices) {
         this.vbo = gl.createBuffer();
@@ -149,162 +233,136 @@ class VertexArray {
 
 }
 
-class PostProcess {
-    constructor() {
-        this.frameBuffer = gl.createFramebuffer();
-        this.textureColorBuffer = gl.createTexture();
-        this.rbo = gl.createRenderbuffer();
-
-        gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer);
-        gl.bindTexture(gl.TEXTURE_2D, this.textureColorBuffer);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 1024, 768, 0, gl.RGB, gl.UNSIGNED_BYTE, null);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.textureColorBuffer, 0);
-
-        gl.bindRenderbuffer(gl.RENDERBUFFER, this.rbo);
-        gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_STENCIL, 1024, 768);
-        gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_STENCIL_ATTACHMENT, gl.RENDERBUFFER, this.rbo);
-
-        if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) !== gl.FRAMEBUFFER_COMPLETE) {
-            console.error('ERROR: Framebuffer is not ok!');
-        }
-
-        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    }
-
-}
-
 function main() {
-    gl.enable(gl.DEPTH_TEST);
     const sphereShader = new Shader('vs', 'fs');
-    const selectedShader = new Shader('svs', 'sfs');
-
     const vertices = new Float32Array([
         -0.5, 0.5, 1.0,
         0.5, 0.5, 1.0,
         0.5, -0.5, 1.0,
         -0.5, -0.5, 1.0
-        
+
     ]);
-
-    const M = 8, N = 16;
-    const sph_n = ((M + 1) * N);
-    let sph_vtx = new Float32Array(3 * sph_n);
-
-    for (let i = 0; i < M + 1; ++i) {
-        const phi = i * Math.PI / M;
-        const y = Math.cos(phi);
-        const r = Math.sin(phi);
-        for (let j = 0; j < N; ++j) {
-            const th = j * 2 * Math.PI / N;
-            const idx = (j + i * N) * 3;
-            sph_vtx[idx] = r * Math.cos(th);
-            sph_vtx[idx + 1] = y;
-            sph_vtx[idx + 2] = r * Math.sin(th);
-
-        }
-    }
-
-    const sph_in = M * (N * 2 + 2) + (M - 1) * 2;
-    const sph_idx = new Uint16Array(sph_in);
-    let idx = 0;
-    for (let i = 0; i < M; ++i) {
-        for (let j = 0; j < N; ++j) {
-            sph_idx[idx++] = i * N + j;
-            sph_idx[idx++] = (i + 1) * N + j;
-        }
-        sph_idx[idx++] = i * N;
-        sph_idx[idx++] = (i + 1) * N;
-
-        if (i < M - 1) {
-            sph_idx[idx++] = (i + 1) * N + (N - 1);
-            sph_idx[idx++] = (i + 1)
-        }
-    }
-
-
-    // console.log(sph_in);
-    // console.log(sph_idx);
-
-
-    const projectionMatrix = float4Perspective(Math.PI / 2, gl.canvas.clientWidth / gl.canvas.clientHeight, 0.1, 100.0);
+    // const projectionMatrix = float4Perspective(Math.PI / 2, gl.canvas.clientWidth / gl.canvas.clientHeight, 0.1, 100.0);
     let angle = 0;
-
-
     sphereShader.enable();
 
-
-    // gl.enable(gl.BLEND);
-    // gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    // gl.enable(gl.CULL_FACE)
-    // gl.cullFace(gl.FRONT)
-    // gl.frontFace(gl.CW);
-
-
-    // gl.enable(gl.CULL_FACE);
-    // gl.cullFace(gl.FRONT)
-    // gl.depthMask(false);
-    // gl.depthFunc(gl.ALWAYS);
-    // gl.enable(gl.STENCIL_TEST);
-
     gl.uniformMatrix4fv(gl.getUniformLocation(sphereShader.program, 'u_rtMatrix'), false, new float4x4().rotate(angle).elements);
-    
-    
-    const postProcessing = new PostProcess();
 
-    gl.bindFramebuffer(gl.FRAMEBUFFER, postProcessing.frameBuffer);
+    const q = new Quaternion(new float3(0, 0, 0), 10);
+    console.log(q);
+    const mt = new float4x4();
+    mt.rotateFromQuatertion(q);
+    console.log(mt);
+
+    const vert = new Float32Array([
+        -.5, -.5, 0,
+        .5, -.5, 0,
+        -.5, .5, 0,
+        .5, .5, 0,
+    ]);
+    const indices = new Uint16Array([
+        0, 1, 2,
+        2, 3, 1
+    ]);
+
+    const mesh = new VertexArray(vert, indices);
+    const proj = mat4.create();
+    mat4.perspective(proj, 45, 4 / 3, 0.1, 100);
+    const mtt = mat4.create();
+    const qt = quat.create()
+    console.log(qt);
+    quat.fromEuler(qt, 45, 45, 45);
+    mat4.fromQuat(mtt, qt);
+    console.log(qt);
+    // const mtt = mat4.
     
+    gl.uniform1i(gl.getUniformLocation(sphereShader.program, 'image'), 0);
+
+
     setInterval(() => {
         const t = Date.now() / 1000;
         const c = Math.cos(t);
         const s = Math.sin(t);
         const viewMatrix = new float4x4().lookAt([2 * c, 0, 2 * s], [0, 0, 0], [0, 1, 0]);
-
-
-        gl.clearColor(0, 0, 0, 1);
-        //
-        // gl.stencilOp(gl.KEEP, gl.KEEP, gl.REPLACE);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
-
-
-        // gl.stencilFunc(gl.ALWAYS, 1, 0xFF);
-        // gl.stencilMask(0xFF);
-        // mesh.render(sph_in);
-
-        // gl.stencilFunc(gl.NOTEQUAL, 1, 0xFF);
-        // gl.stencilMask(0x00);
-        // gl.disable(gl.DEPTH_TEST);
-        gl.enable(gl.DEPTH_TEST);
-        const mesh = new VertexArray(sph_vtx, sph_idx);
-        sphereShader.enable();
         gl.uniformMatrix4fv(gl.getUniformLocation(sphereShader.program, 'u_viewMatrix'), false, viewMatrix.elements);
-        gl.uniformMatrix4fv(gl.getUniformLocation(sphereShader.program, 'u_projMatrix'), false, projectionMatrix);
+        gl.uniformMatrix4fv(gl.getUniformLocation(sphereShader.program, 'u_projMatrix'), false, proj);
+        gl.uniformMatrix4fv(gl.getUniformLocation(sphereShader.program, 'u_modelMatrix'), false, mtt);
         gl.uniform1f(gl.getUniformLocation(sphereShader.program, 'z'), 0);
-        mesh.render(sph_in);
 
+        gl.clearColor(0, 0, 0, .5);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        mesh.render(6);
 
-        selectedShader.enable();
-        gl.uniformMatrix4fv(gl.getUniformLocation(selectedShader.program, 'u_viewMatrix'), false, viewMatrix.elements);
-        gl.uniformMatrix4fv(gl.getUniformLocation(selectedShader.program, 'u_projMatrix'), false, projectionMatrix);
-        gl.uniform1f(gl.getUniformLocation(selectedShader.program, 'z'), 0);
-        const window = new VertexArray(vertices, new Uint16Array([
-            0, 1, 2,
-            2, 3, 0
-        ]));
-        window.render(6);
-        selectedShader.disable();
-        
-        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-        gl.disable(gl.DEPTH_TEST);
-        gl.clearColor(1, 1, 1, 1);
-        
-
-    }, 100)
+    }, 20)
 
 }
 
-main();
+
+class Quaternion {
+    constructor(vector = new float4(), a = 0) {
+        a = a / 360. * Math.PI * 2;
+
+        this.x = vector.x * Math.sin(a / 2);
+        this.y = vector.y * Math.sin(a / 2);
+        this.z = vector.z * Math.sin(a / 2);
+        this.w = Math.cos(a / 2);
+    }
+
+    static fromEulerAngles(angles = new float3()) {
+        const pitch = new Quaternion(new float3(1., 0, 0), angles.x); // тангаж
+        const yaw = new Quaternion(new float3(0, 1., 0), angles.y); // рыскание
+        const roll = new Quaternion(new float3(0, 0, 1.), angles.z); // крен
+        return yaw.multiplyQuat(roll).multiplyQuat(pitch);
+    }
+
+    from4Coords(x, y, z, w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+        return this;
+    }
+
+    multiplyQuat(quat) {
+        const res = new Quaternion();
+        return this.normalize(res.from4Coords(
+            (((this.w * quat.x) + (this.x * quat.w)) + (this.y * quat.z)) - (this.z * quat.y),
+            (((this.w * quat.y) + (this.y * quat.w)) + (this.z * quat.x)) - (this.x * quat.z),
+            (((this.w * quat.z) + (this.z * quat.w)) + (this.x * quat.y)) - (this.y * quat.x),
+            (((this.w * quat.w) + (this.x * quat.x)) + (this.y * quat.y)) - (this.z * quat.z),
+        ));
+
+    }
+
+    multiplyScalar(scalar) {
+        return new Quaternion(this.x * scalar, this.y * scalar, this.z * scalar, this.w * scalar);
+    }
+
+
+    normalize(quat) {
+        const lenSqr = this._norm(quat);
+        const lenInv = Math.sqrt(lenSqr);
+        return this.multiplyScalar(lenInv);
+    }
+
+    _norm(quat) {
+        let res = quat.x * quat.x;
+        res += quat.y * quat.y;
+        res += quat.z * quat.z;
+        res += quat.w * quat.w;
+        return res;
+    }
+
+
+}
+
+const texture = new Texture('./1.jpg');
+texture.create().then(() => {
+    texture.bind();
+    main();
+});
+
+// const qq = Quaternion.fromEulerAngles(new float3(90, 0, 0));
 
 function float4Perspective(fovy, aspect, near, far) {
     const f = 1 / Math.tan(fovy / 2);
@@ -317,3 +375,4 @@ function float4Perspective(fovy, aspect, near, far) {
         0, 0, 2 * far * near * nf, 0
     ])
 }
+
