63f235955e44bb5792f82f391053e17d2654b211
RPG59 <RPG591@mail.ru>
2020-01-19 23:25:45 +0300

    cubemap

--- src/main.ts
+++ src/main.ts
@@ -161,22 +161,22 @@ class float4x4 {
 		return this;
 	};
 
-    multiply(matrix: float4x4) {
-        const res = new Array(16).fill(0);
+	multiply(matrix: float4x4) {
+		const res = new Array(16).fill(0);
 
-        for (let y = 0; y < 4; ++y) {
-            for (let x = 0; x < 4; ++x) {
-                let sum = 0;
-                for (let e = 0; e < 4; ++e) {
-                    sum += this.elements[x + e * 4] * matrix.elements[e + y * 4];
-                }
-                res[x + y * 4] = sum;
-            }
-        }
-        this.elements = new Float32Array(res);
+		for (let y = 0; y < 4; ++y) {
+			for (let x = 0; x < 4; ++x) {
+				let sum = 0;
+				for (let e = 0; e < 4; ++e) {
+					sum += this.elements[x + e * 4] * matrix.elements[e + y * 4];
+				}
+				res[x + y * 4] = sum;
+			}
+		}
+		this.elements = new Float32Array(res);
 
-        return this;
-    }
+		return this;
+	}
 }
 
 class VertexArray {
@@ -222,50 +222,95 @@ function main() {
 	gl.enable(gl.DEPTH_TEST);
 	const sphereShader = new Shader('vs', 'fs');
 	const lightShader = new Shader('vs_lighting', 'fs_lighting');
+	const skyboxShader = new Shader('vs_skybox', 'fs_skybox');
+	
+	let skybox_vtx = new Float32Array([
+		-1.0,  1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0,  1.0, -1.0,
+		-1.0,  1.0, -1.0,
+
+		-1.0, -1.0,  1.0,
+		-1.0, -1.0, -1.0,
+		-1.0,  1.0, -1.0,
+		-1.0,  1.0, -1.0,
+		-1.0,  1.0,  1.0,
+		-1.0, -1.0,  1.0,
+
+		1.0, -1.0, -1.0,
+		1.0, -1.0,  1.0,
+		1.0,  1.0,  1.0,
+		1.0,  1.0,  1.0,
+		1.0,  1.0, -1.0,
+		1.0, -1.0, -1.0,
+
+		-1.0, -1.0,  1.0,
+		-1.0,  1.0,  1.0,
+		1.0,  1.0,  1.0,
+		1.0,  1.0,  1.0,
+		1.0, -1.0,  1.0,
+		-1.0, -1.0,  1.0,
+
+		-1.0,  1.0, -1.0,
+		1.0,  1.0, -1.0,
+		1.0,  1.0,  1.0,
+		1.0,  1.0,  1.0,
+		-1.0,  1.0,  1.0,
+		-1.0,  1.0, -1.0,
+
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0,  1.0,
+		1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		-1.0, -1.0,  1.0,
+		1.0, -1.0,  1.0
+	]);
 
 
 	let sph_vtx = new Float32Array([
-		-0.5, -0.5, -0.5,  0.0,  0.0, -1.0,  0.0,  0.0,
-		0.5, -0.5, -0.5,  0.0,  0.0, -1.0,  1.0,  0.0,
-		0.5,  0.5, -0.5,  0.0,  0.0, -1.0,  1.0,  1.0,
-		0.5,  0.5, -0.5,  0.0,  0.0, -1.0,  1.0,  1.0,
-		-0.5,  0.5, -0.5,  0.0,  0.0, -1.0,  0.0,  1.0,
-		-0.5, -0.5, -0.5,  0.0,  0.0, -1.0,  0.0,  0.0,
+		-0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 0.0,
+		0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 1.0, 0.0,
+		0.5, 0.5, -0.5, 0.0, 0.0, -1.0, 1.0, 1.0,
+		0.5, 0.5, -0.5, 0.0, 0.0, -1.0, 1.0, 1.0,
+		-0.5, 0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 1.0,
+		-0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 0.0,
 
-		-0.5, -0.5,  0.5,  0.0,  0.0,  1.0,  0.0,  0.0,
-		0.5, -0.5,  0.5,  0.0,  0.0,  1.0,  1.0,  0.0,
-		0.5,  0.5,  0.5,  0.0,  0.0,  1.0,  1.0,  1.0,
-		0.5,  0.5,  0.5,  0.0,  0.0,  1.0,  1.0,  1.0,
-		-0.5,  0.5,  0.5,  0.0,  0.0,  1.0,  0.0,  1.0,
-		-0.5, -0.5,  0.5,  0.0,  0.0,  1.0,  0.0,  0.0,
+		-0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 0.0,
+		0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 0.0,
+		0.5, 0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 1.0,
+		0.5, 0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 1.0,
+		-0.5, 0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 1.0,
+		-0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 0.0,
 
-		-0.5,  0.5,  0.5, -1.0,  0.0,  0.0,  1.0,  0.0,
-		-0.5,  0.5, -0.5, -1.0,  0.0,  0.0,  1.0,  1.0,
-		-0.5, -0.5, -0.5, -1.0,  0.0,  0.0,  0.0,  1.0,
-		-0.5, -0.5, -0.5, -1.0,  0.0,  0.0,  0.0,  1.0,
-		-0.5, -0.5,  0.5, -1.0,  0.0,  0.0,  0.0,  0.0,
-		-0.5,  0.5,  0.5, -1.0,  0.0,  0.0,  1.0,  0.0,
+		-0.5, 0.5, 0.5, -1.0, 0.0, 0.0, 1.0, 0.0,
+		-0.5, 0.5, -0.5, -1.0, 0.0, 0.0, 1.0, 1.0,
+		-0.5, -0.5, -0.5, -1.0, 0.0, 0.0, 0.0, 1.0,
+		-0.5, -0.5, -0.5, -1.0, 0.0, 0.0, 0.0, 1.0,
+		-0.5, -0.5, 0.5, -1.0, 0.0, 0.0, 0.0, 0.0,
+		-0.5, 0.5, 0.5, -1.0, 0.0, 0.0, 1.0, 0.0,
 
-		0.5,  0.5,  0.5,  1.0,  0.0,  0.0,  1.0,  0.0,
-		0.5,  0.5, -0.5,  1.0,  0.0,  0.0,  1.0,  1.0,
-		0.5, -0.5, -0.5,  1.0,  0.0,  0.0,  0.0,  1.0,
-		0.5, -0.5, -0.5,  1.0,  0.0,  0.0,  0.0,  1.0,
-		0.5, -0.5,  0.5,  1.0,  0.0,  0.0,  0.0,  0.0,
-		0.5,  0.5,  0.5,  1.0,  0.0,  0.0,  1.0,  0.0,
+		0.5, 0.5, 0.5, 1.0, 0.0, 0.0, 1.0, 0.0,
+		0.5, 0.5, -0.5, 1.0, 0.0, 0.0, 1.0, 1.0,
+		0.5, -0.5, -0.5, 1.0, 0.0, 0.0, 0.0, 1.0,
+		0.5, -0.5, -0.5, 1.0, 0.0, 0.0, 0.0, 1.0,
+		0.5, -0.5, 0.5, 1.0, 0.0, 0.0, 0.0, 0.0,
+		0.5, 0.5, 0.5, 1.0, 0.0, 0.0, 1.0, 0.0,
 
-		-0.5, -0.5, -0.5,  0.0, -1.0,  0.0,  0.0,  1.0,
-		0.5, -0.5, -0.5,  0.0, -1.0,  0.0,  1.0,  1.0,
-		0.5, -0.5,  0.5,  0.0, -1.0,  0.0,  1.0,  0.0,
-		0.5, -0.5,  0.5,  0.0, -1.0,  0.0,  1.0,  0.0,
-		-0.5, -0.5,  0.5,  0.0, -1.0,  0.0,  0.0,  0.0,
-		-0.5, -0.5, -0.5,  0.0, -1.0,  0.0,  0.0,  1.0,
+		-0.5, -0.5, -0.5, 0.0, -1.0, 0.0, 0.0, 1.0,
+		0.5, -0.5, -0.5, 0.0, -1.0, 0.0, 1.0, 1.0,
+		0.5, -0.5, 0.5, 0.0, -1.0, 0.0, 1.0, 0.0,
+		0.5, -0.5, 0.5, 0.0, -1.0, 0.0, 1.0, 0.0,
+		-0.5, -0.5, 0.5, 0.0, -1.0, 0.0, 0.0, 0.0,
+		-0.5, -0.5, -0.5, 0.0, -1.0, 0.0, 0.0, 1.0,
 
-		-0.5,  0.5, -0.5,  0.0,  1.0,  0.0,  0.0,  1.0,
-		0.5,  0.5, -0.5,  0.0,  1.0,  0.0,  1.0,  1.0,
-		0.5,  0.5,  0.5,  0.0,  1.0,  0.0,  1.0,  0.0,
-		0.5,  0.5,  0.5,  0.0,  1.0,  0.0,  1.0,  0.0,
-		-0.5,  0.5,  0.5,  0.0,  1.0,  0.0,  0.0,  0.0,
-		-0.5,  0.5, -0.5,  0.0,  1.0,  0.0,  0.0,  1.0
+		-0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0,
+		0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 1.0, 1.0,
+		0.5, 0.5, 0.5, 0.0, 1.0, 0.0, 1.0, 0.0,
+		0.5, 0.5, 0.5, 0.0, 1.0, 0.0, 1.0, 0.0,
+		-0.5, 0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 0.0,
+		-0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0
 	]);
 
 	// @ts-ignore
@@ -275,14 +320,8 @@ function main() {
 
 	const projectionMatrix = new Float32Array(new Array(16).fill(0));
 
-	glm.mat4.perspective(projectionMatrix, Math.PI / 2,  gl.canvas.clientWidth / gl.canvas.clientHeight, .1 , 100.);
-	console.log(projectionMatrix);
-
-	// // @ts-ignore
-	// const VAO = gl.createVertexArray();
-	// // @ts-ignore
-	// gl.bindVertexArray(VAO);
-
+	glm.mat4.perspective(projectionMatrix, Math.PI / 4, gl.canvas.clientWidth / gl.canvas.clientHeight, .1, 100.);
+	
 	const buff1 = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, buff1);
 	gl.bufferData(gl.ARRAY_BUFFER, sph_vtx, gl.STATIC_DRAW);
@@ -292,8 +331,14 @@ function main() {
 	gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 8 * 4, 3 * 4);
 	gl.enableVertexAttribArray(2);
 	gl.vertexAttribPointer(2, 3, gl.FLOAT, false, 8 * 4, 6 * 4);
-
 	
+	const skyboxBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, skyboxBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, skybox_vtx, gl.STATIC_DRAW);
+	gl.enableVertexAttribArray(3);
+	gl.vertexAttribPointer(3, 3, gl.FLOAT, false, 0, 0);
+
+
 	// const modelMatrix = new Float32Array(new Array(16).fill(0));
 	// modelMatrix[0] = .1;
 	// modelMatrix[5] = .1;
@@ -301,27 +346,27 @@ function main() {
 	// modelMatrix[15] = 1;
 
 
-
-	
-
 	// glm.mat4.scale(modelMatrix, new Float32Array(new Array(16).fill(0)))
 
-	const lightPosition = [12, 2, 0];
+	const lightPosition = [1.5, 0, 0];
 	sphereShader.enable();
-	
-	
-	
+
+
 	gl.uniform1i(gl.getUniformLocation(sphereShader.program, 'material.diffuse'), 2);
 	gl.uniform1i(gl.getUniformLocation(sphereShader.program, 'material.specular'), 3);
 	gl.uniform1f(gl.getUniformLocation(sphereShader.program, 'material.shininess'), 32.);
 
 	gl.uniform3fv(gl.getUniformLocation(sphereShader.program, 'light.position'), lightPosition);
-	gl.uniform3f(gl.getUniformLocation(sphereShader.program, 'light.ambient'), .3,.2,.1);
+	gl.uniform3f(gl.getUniformLocation(sphereShader.program, 'light.ambient'), .3, .2, .1);
 	gl.uniform3f(gl.getUniformLocation(sphereShader.program, 'light.diffuse'), .5, .7, .1);
 	gl.uniform3f(gl.getUniformLocation(sphereShader.program, 'light.specular'), 1., 1., 1.);
 	gl.uniform1f(gl.getUniformLocation(sphereShader.program, 'light.constant'), 1.);
 	gl.uniform1f(gl.getUniformLocation(sphereShader.program, 'light.linear'), .045);
 	gl.uniform1f(gl.getUniformLocation(sphereShader.program, 'light.quadratic'), .0075);
+
+	gl.uniform1f(gl.getUniformLocation(sphereShader.program, 'spotlight.cutOff'), Math.cos(Math.PI / 8));
+	gl.uniform3fv(gl.getUniformLocation(sphereShader.program, 'spotlight.direction'), lightPosition);
+	gl.enable(gl.DEPTH_TEST);
 
 	const render = () => {
 		sphereShader.enable();
@@ -329,16 +374,17 @@ function main() {
 		// const t = 6.2;
 		const c = Math.cos(t);
 		const s = Math.sin(t);
-		
 
-        const tmpMat2 = new float4x4().translate(lightPosition);
 
-	    const lightModelMatrix = new float4x4().scale(.1, .1, .1).multiply(tmpMat2);
-		const cameraPosition = [2*c, 0, 2*s];
-		// const cameraPosition = [0, 0, 3];
+		const tmpMat2 = new float4x4().translate(lightPosition);
+
+		// const lightModelMatrix = new float4x4().scale(.1, .1, .1).multiply(tmpMat2);
+		const lightModelMatrix = new float4x4().translate(lightPosition).multiply(new float4x4().scale(.1, .1, .1));
+		const cameraPosition = [2* c , 0, 2 *s ];
+		// const cameraPosition = [0, 1, 2];
 		const viewMatrix = new float4x4().lookAt(cameraPosition, [0, 0, 0], [0, 1, 0]);
 		// const viewMatrix = new float4x4().lookAt([2*Math.sin(1), 1, 2*Math.cos(1)], [0,0,0], [0, 1, 0]);
-		
+
 		// const viewMatrix = new Float32Array(new Array(16).fill(0));
 		// glm.mat4.lookAt(
 		// 	viewMatrix,
@@ -347,11 +393,16 @@ function main() {
 		// 	new Float32Array([0, 0, -1]),n
 		// 	new Float32Array([0, 1, 0])
 		// );
-		const modelMatrix = new float4x4();      
+		const modelMatrix = new float4x4();
+		modelMatrix.scale(.5, .5, .5);
+		const skyboxModelMatrix = new float4x4();
+		skyboxModelMatrix.scale(2, 2, 2)
 
 		gl.clearColor(0, 0, 0, 1);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
-		gl.enable(gl.DEPTH_TEST);
+
+		
+		sphereShader.enable();
 		gl.uniformMatrix4fv(gl.getUniformLocation(sphereShader.program, 'u_viewMatrix'), false, viewMatrix.elements);
 		gl.uniformMatrix4fv(gl.getUniformLocation(sphereShader.program, 'u_projMatrix'), false, projectionMatrix);
 		gl.uniformMatrix4fv(gl.getUniformLocation(sphereShader.program, 'u_modelMatrix'), false, modelMatrix.elements);
@@ -361,22 +412,22 @@ function main() {
 		gl.uniform3fv(gl.getUniformLocation(sphereShader.program, 'viewPos'), new Float32Array(cameraPosition));
 
 		gl.drawArrays(gl.TRIANGLES, 0, 35);
-
-		gl.uniformMatrix4fv(gl.getUniformLocation(sphereShader.program, 'u_modelMatrix'), false, modelMatrix.translate([-1.5, 1.5, 0]).elements);
-		gl.drawArrays(gl.TRIANGLES, 0, 35);
-
-		gl.uniformMatrix4fv(gl.getUniformLocation(sphereShader.program, 'u_modelMatrix'), false, modelMatrix.translate([-1.5 * 2, -2, 0]).elements);
-		gl.drawArrays(gl.TRIANGLES, 0, 35);
-
-		lightShader.enable();
-		gl.uniformMatrix4fv(gl.getUniformLocation(lightShader.program, 'u_viewMatrix'), false, viewMatrix.elements);
-		gl.uniformMatrix4fv(gl.getUniformLocation(lightShader.program, 'u_projMatrix'), false, projectionMatrix);
-		gl.uniformMatrix4fv(gl.getUniformLocation(lightShader.program, 'u_modelMatrix'), false, lightModelMatrix.elements);
+		
+		gl.depthFunc(gl.LEQUAL)
+		skyboxShader.enable();
+		gl.uniformMatrix4fv(gl.getUniformLocation(skyboxShader.program, 'u_viewMatrix'), false, viewMatrix.elements);
+		gl.uniformMatrix4fv(gl.getUniformLocation(skyboxShader.program, 'u_projMatrix'), false, projectionMatrix);
+		gl.uniformMatrix4fv(gl.getUniformLocation(skyboxShader.program, 'u_modelMatrix'), false, skyboxModelMatrix.elements);
 		gl.drawArrays(gl.TRIANGLES, 0, 36);
+		gl.depthFunc(gl.LESS);
 
-
-
-
+		// lightShader.enable();
+		// gl.uniformMatrix4fv(gl.getUniformLocation(lightShader.program, 'u_viewMatrix'), false, viewMatrix.elements);
+		// gl.uniformMatrix4fv(gl.getUniformLocation(lightShader.program, 'u_projMatrix'), false, projectionMatrix);
+		// gl.uniformMatrix4fv(gl.getUniformLocation(lightShader.program, 'u_modelMatrix'), false, lightModelMatrix.elements);
+		// gl.drawArrays(gl.TRIANGLES, 0, 36);
+		
+		
 		requestAnimationFrame(render)
 	}
 
@@ -391,6 +442,14 @@ function main() {
 	const texture2 = await creatTexture('NormalMap (2).png');
 	const texture3 = await creatTexture('container2.png');
 	const texture4 = await creatTexture('container2_specular.png');
+	const cubemap1 = await createCubemap([
+		"cubemap/right.jpg",
+		"cubemap/left.jpg",
+		"cubemap/top.jpg",
+		"cubemap/bottom.jpg",
+		"cubemap/front.jpg",
+		"cubemap/back.jpg"
+	]);
 
 	gl.activeTexture(gl.TEXTURE0);
 	gl.bindTexture(gl.TEXTURE_2D, texture1);
@@ -400,9 +459,53 @@ function main() {
 	gl.bindTexture(gl.TEXTURE_2D, texture3);
 	gl.activeTexture(gl.TEXTURE3);
 	gl.bindTexture(gl.TEXTURE_2D, texture4);
-
+	// gl.activeTexture(gl.TEXTURE4);
+	// gl.bindTexture(gl.TEXTURE4, cubemap1);
 	main();
-})()
+})();
+
+async function createCubemap(paths: string[]): Promise<WebGLTexture> {
+	return new Promise(res => {
+		const texture = gl.createTexture();
+		const imgs: any = new Array(6).fill(0).map(x => new Image());
+		const promises = [];
+
+		imgs.forEach((img, i) => {
+			img.src = paths[i];
+			const promise = new Promise(res => {
+				img.onload = () => {
+					res();
+				}
+			});
+			promises.push(promise);
+		});
+
+		Promise.all(promises).then(() => {
+			gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+			
+			paths.forEach((path, i) => {
+				gl.texImage2D(
+					gl.TEXTURE_CUBE_MAP_POSITIVE_X + i,
+					0,
+					gl.RGB,
+					gl.RGB,
+					gl.UNSIGNED_BYTE,
+					imgs[i]
+				);
+			});
+
+			gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+			gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+			gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+			// @ts-ignore
+			gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE);
+			
+			res(texture);
+		});
+
+	});
+}
 
 
 async function creatTexture(path): Promise<WebGLTexture> {
